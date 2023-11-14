8cb9b3540d242d09da4044be8d5a8ec74d637927
MikolajSzafraniecUPDS <mikolaj.szafraniec.upds@gmail.com>
2023-11-14 14:54:18 +0100

    'Submodule numpy cloned'

--- run_etl.py
+++ run_etl.py
@@ -10,5 +10,5 @@ from config.repos_to_analyze import REPOS_TO_ANALYZE
 from config.repos_to_analyze import SUBMODULES_DIR
 
 if __name__ == "__main__":
-    #get_repos(repos_list=REPOS_TO_ANALYZE, submodules_dir=SUBMODULES_DIR)
-    delete_repos(repos_dir=SUBMODULES_DIR)
+    get_repos(repos_list=REPOS_TO_ANALYZE, submodules_dir=SUBMODULES_DIR)
+    #delete_repos(repos_dir=SUBMODULES_DIR)
