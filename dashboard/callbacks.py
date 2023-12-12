09bdb01042377eb2d7bf9e0a7a7628a8c85dfc8a
MikolajSzafraniecUPDS <mikolaj.szafraniec.upds@gmail.com>
2023-12-12 20:56:43 +0100

    Copied app.py for development purpose, cleaned main requirements file

--- dashboard/callbacks.py
+++ dashboard/callbacks.py
@@ -16,7 +16,7 @@ from database.get_db_engine import get_db_engine
 from config.config import TOP_N_CONTRIBUTORS_DASHBOARD, TOP_N_WORDS_DASHBOARD, DASHBOARD_SD_OUTLIERS_BORDER
 from wordcloud import WordCloud
 
-_ENGINE = get_db_engine(inside_compose_network=True)
+_ENGINE = get_db_engine(inside_compose_network=False)
 
 
 def _render_word_cloud_image(freq_table: pd.DataFrame):
